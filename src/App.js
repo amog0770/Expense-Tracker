327d5056971d1a31000bc19627c0ff656d26519a
Amog Swami <amog.swami20@pccoepune.org>
2024-04-02 20:21:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Header } from './Component/Header';
+import{Balance} from './Component/Balance'
+import {IncomeExpenses} from './Component/IncomeExpense'
+import {TransactionList} from './Component/TransactionList'
+import{AddTransaction} from './Component/AddTransaction'
+import{GlobalProvider} from './Context/GlobalState'
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+    <Header/>
+    <div className="container">
+     <Balance/>
+     <IncomeExpenses/>
+     <TransactionList/>
+     <AddTransaction/>
+    </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
+
